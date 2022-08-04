e40ed809f4e59aedcc02dbf6693577f04e2804b7
Muhammad Zarith <52603129+yedd77@users.noreply.github.com>
2022-08-05 04:06:05 +0800

    V1.0.001 added ingredient layout

---
+++ app/src/main/java/com/example/recipely/ingred_veg.java
@@ -0,0 +1,14 @@
+package com.example.recipely;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ingred_veg extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ingred_veg);
+    }
+}
